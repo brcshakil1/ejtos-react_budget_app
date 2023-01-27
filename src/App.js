643c64a9a69084658cc158911d8ce02b15d5712a
brcshakil1 <brcshakil72@gmail.com>
2023-01-27 15:06:49 +0600

    IBM react course's project

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Add code to import the components
 import { AppProvider } from './context/AppContext';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import AllocationForm from './components/AllocationForm';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                {/* Add the code here to add the components          */}
+                <Budget />
+                <Remaining />
+                <ExpenseItem />
+                <ExpenseList />
+                <ExpenseTotal />
+                <AllocationForm />
             </div>
         </AppProvider>
     );
